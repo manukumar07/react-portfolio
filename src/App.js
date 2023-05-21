603a877da5aadbd9944dad8be9151e2e52c1c9a5
Manu Kumar Pal <manukumarpal34@gmail.com>
2023-05-21 21:25:29 +0530

    adding router and components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      App
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+       <Route  Exact path="/" element={<home/> } />
+       <Route path="/about"  element={<about/>} />
+       <Route path="/skills" element={<skills/>} />
+       <Route path="/project" element={<projects/> } />
+       <Route path="/contact" element={<contact-us />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import "./Navbar.css"
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <>
-        <div className='main-navbar'>
-
-        </div>
+     <nav className='nav'>
+     
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/skills">Skills</Link>
+        </li>
+        <li>
+          <Link to="/project">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact_us</Link>
+        </li>
+      </ul>
+    </nav>
     </>
   )
 }
