95173e5001b1625fbf8c23b447b6c65611b5b78e
Manu Kumar Pal <manukumarpal34@gmail.com>
2023-06-03 20:22:20 +0530

    modifify navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 
 import './App.css';
+import Contact from './components/Contact';
 import Navbar from './components/navbar/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-       <Route  Exact path="/" element={<home/> } />
-       <Route path="/about"  element={<about/>} />
-       <Route path="/skills" element={<skills/>} />
-       <Route path="/project" element={<projects/> } />
-       <Route path="/contact" element={<contact-us />} />
-      </Routes>
-    </Router>
+   <>
+    {/* <Navbar /> */}
+    <Contact />
+   </>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,32 +1,43 @@
-import React from 'react'
-import "./Navbar.css"
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 const Navbar = () => {
-  return (
-    <>
-     <nav className='nav'>
-     
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/skills">Skills</Link>
-        </li>
-        <li>
-          <Link to="/project">Projects</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact_us</Link>
-        </li>
-      </ul>
-    </nav>
-    </>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Navbar
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="/">
+          My Logo
+        </a>
+        <button
+          className={`navbar-burger ${isOpen ? 'is-active' : ''}`}
+          onClick={toggleNavbar}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+      <div className={`navbar-menu ${isOpen ? 'is-active' : ''}`}>
+        <div className="navbar-start">
+          <a className="navbar-item" href="/">
+            Home
+          </a>
+          <a className="navbar-item" href="/about">
+            About
+          </a>
+          <a className="navbar-item" href="/contact">
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
